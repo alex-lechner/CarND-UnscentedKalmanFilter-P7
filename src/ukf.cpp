b91833d1e284d7d245eead84680971bfd8e12d21
ALEX-PC\Alexa <office@alexanderlechner.com>
2017-10-20 20:14:38 +0200

    tuned noise parameters & finished project

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -23,10 +23,10 @@ UKF::UKF() {
     P_ = MatrixXd(5, 5);
 
     // Process noise standard deviation longitudinal acceleration in m/s^2
-    std_a_ = 2.5;
+    std_a_ = 5;
 
     // Process noise standard deviation yaw acceleration in rad/s^2
-    std_yawdd_ = 0.12;
+    std_yawdd_ = 1.5;
 
     // Laser measurement noise standard deviation position1 in m
     std_laspx_ = 0.15;
